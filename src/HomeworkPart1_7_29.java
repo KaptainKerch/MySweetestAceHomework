49443ee113a6571a9a24e8f7f51dfb92677227d8
Jason <jasonh1028@hotmail.com>
2016-11-05 18:33:11 -0500

    Testing to see if my branch works.

--- src/HomeworkPart1_7_29.java
+++ src/HomeworkPart1_7_29.java
@@ -2,7 +2,8 @@
 public class HomeworkPart1_7_29 {
 
 	public static void main(String[] args) {
-		System.out.print("Hello Jason");
+		System.out.println("Hello Jason");
+		System.out.println("This is a Test.");
 	}
 
 }
